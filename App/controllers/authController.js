c1f30136f7e5feb46ebc454be4e206901117e38f
Paras Sahu <paras2115s@gmail.com>
2026-06-15 16:45:29 +0530

    route and controller

---
+++ App/controllers/authController.js
@@ -0,0 +1,6 @@
+
+const signUp = (req,res)=>{
+    res.send("user signed")
+}
+
+module.exports ={signUp}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const authRouter = require("./App/routes/authRouter")
 require("dotenv").config()
 const app = express()
 app.use(express.json())
 
-
+app.use("/api/user",authRouter)
 app.get("/",(req,res)=>{
     res.send("Backend is Running")
 })
