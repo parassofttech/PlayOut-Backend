002c11d4cc1c562e09461fe5980cd04de486b57a
Paras Sahu <paras2115s@gmail.com>
2026-06-15 16:52:34 +0530

    signup controller

--- App/controllers/authController.js
+++ App/controllers/authController.js
@@ -1,6 +1,18 @@
+const userModel = require("../models/User")
 
 const signUp = (req,res)=>{
-    res.send("user signed")
+
+    let {name,email, password} = req.body
+    let user = new userModel({
+        name,
+        email,
+        password
+    })
+    user.save().then(()=>{
+        res.send({status:1,message:"signUp sucessfully"})
+    }).catch((err)=>{
+        res.send({status:1,message:"signUp does nat sucessfully"})
+    })
 }
 
 module.exports ={signUp}
